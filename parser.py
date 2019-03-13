348e6c9b8450a433b507d8f919a346a7ba727307
farleyoli <farleyoli@hotmail.com>
2019-03-13 13:52:24 +0900

    add redundancy, get raw pronunciation, get raw definitions

--- parser.py
+++ parser.py
@@ -36,37 +36,118 @@ class WiktionaryXMLHandler(xml.sax.ContentHandler):
         if self.isTitle:
             self.title += content
 
-def getEnglish(textRaw):
+def getEnglish(raw):
     # if no English part in text, returns empty string
     # otherwise, returns string with raw english
 
     substr = "==English=="
-    startPos = textRaw.find(substr)
+    startPos = raw.find(substr)
     if startPos == -1:
         return ""
     startPos = startPos + 11
 
-    isThereOtherLang = bool(re.search("[^=]==[A-Z]", textRaw[startPos:]))
-    if(isThereOtherLang):
-        endPos = re.search("[^=]==[^=]", textRaw[startPos:]).start()+1
+    # FM: four minus
+    isThereFM = bool(re.search("[^-]----[^-]", raw[startPos:]))  
+    if(isThereFM):
+        endPos = re.search("[^-]----[^-]", raw[startPos:]).start()+1
         endPos += startPos
         #print("(there is at least another lang)")
-        return textRaw[startPos:endPos]
-    # english goes until end of string
-    return textRaw[startPos:]
+        return raw[startPos:endPos]
 
+
+    # add redundancy
+    # (DE: double equal)
+    isThereDE = bool(re.search("[^=]==[^=]", raw[startPos:]))
+    if(isThereDE):
+        endPos = re.search("[^=]==[^=]", raw[startPos:]).start()+1
+        endPos += startPos
+        #print("(there is at least another lang)")
+        return raw[startPos:endPos]
+    # english goes until end of string
+    return raw[startPos:]
+
+def getPron(raw):
+    # receives the raw english string and returns the raw
+    # pronunciation string
+    # returns the empty string if there is no pronunciation (or
+    # the formatting of the pronunciation is unorthodox)
+
+    substr = "===Pronunciation==="
+    startPos = raw.find(substr)
+    if startPos == -1:
+        return ""
+    startPos = startPos + 19
+
+    # it stands to reason that the pronunciation is not going to be
+    # the last = * = type of structure in file
+    isThereFM = bool(re.search("===", raw[startPos:]))  
+    if(isThereFM):
+        endPos = re.search("===", raw[startPos:]).start()
+        endPos += startPos
+        return raw[startPos:endPos]
+    return ""
+
+def getDfn(raw):
+    # receives the raw english string and returns a list with 
+    # couples of the form (class, defition string)
+    # returns the empty list if there is no definition (or
+    # the formatting of the definition is unorthodox)
+
+    result = []
+
+    classOfWords = ["Noun", "Verb", "Adjective", "Adverb", "Pronoun", 
+            "Preposition", "Conjuction", "Article", "Interjection"]
+    # Add "Particle" class? Perhaps later
+
+    for cl in classOfWords:
+        # when there are multiple etymologies, they may use four =,
+        isFourE = False
+
+        substr = "====" + cl + "===="
+        startPos = raw.find(substr)
+        if startPos != -1:
+            startPos += len(substr)
+            isFourE = True
+
+        if(not isFourE):
+            substr = "===" + cl + "==="
+            startPos = raw.find(substr)
+            if startPos == -1:
+                continue
+            startPos += len(substr)
+
+        isThereE = bool(re.search("==", raw[startPos:]))   # E: equal
+        if(isThereE):
+            endPos = re.search("==", raw[startPos:]).start()
+            endPos += startPos
+            result.append((cl, raw[startPos:endPos]))
+        else:
+            result.append((cl , raw[startPos:]))
+
+    return result
+        
 
 def processText(textRaw, title):
-    #print(title)
 
     # get english string
     englishRaw = getEnglish(textRaw)
-    print (englishRaw)
-    print("\n------hmm------\n")
+    #print (englishRaw)
 
     # get pronunciation raw string
+    pronRaw = getPron(englishRaw)
+    #print(pronRaw)
 
-    # get definition raw string
+    # get definition raw strings
+    # list of couples (class, def), where class in {noun, verb, etc}
+    dfnRaw = getDfn(englishRaw)
+    for tpl in dfnRaw:
+        if (len(tpl) != 0):
+            (cl, df) = tpl
+            print(title)
+            print(cl)
+            print("---hmm---")
+            print(df)
+
 
     # pronunciation: raw string -> nice string
 
